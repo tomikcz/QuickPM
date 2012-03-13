3cb927bf9b6b831ffbe5b92d6be15b7b2f5dd4dc
Tomas Bryja <tomas@krnov.cz>
2012-03-13 23:51:03 +0100

    modified:   src/cz/xmc/QuickPM/QuickPMPlayerListener.java

--- src/cz/xmc/QuickPM/QuickPMPlayerListener.java
+++ src/cz/xmc/QuickPM/QuickPMPlayerListener.java
@@ -1,30 +1,37 @@
 package cz.xmc.QuickPM;
-
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
-//import org.bukkit.entity.Player;
-//import org.bukkit.ChatColor;
-//import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChatEvent;
-import java.util.logging.Logger;
 
 public class QuickPMPlayerListener implements Listener {
     public QuickPM plugin;
-    public Logger logg;
-    
-
-    public QuickPMPlayerListener(QuickPM plugin) {
-		this.plugin = plugin;
-
-		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+	public String msg;     
+	
+	public QuickPMPlayerListener(QuickPM plugin) {
+	    this.plugin = plugin;
+	    plugin.getServer().getPluginManager().registerEvents(this, plugin);
 	}
     
     @EventHandler(priority = EventPriority.LOWEST)
-	public void onPlayerChat(PlayerChatEvent event) {           	
-    	String[] msg = event.getMessage().split(" ");
-    	event.getPlayer().damage(1);
-    	event.getPlayer().sendRawMessage("zkouska");
-    	return;
+	public void onPlayerChat(PlayerChatEvent event) {
+    	msg = event.getMessage();
+    	String[] msgp = msg.split(" ");
+    	String nick = msgp[0].toString();
+    	if (nick.endsWith(":")){
+    	  nick=nick.substring(0,nick.length()-1);
+    	  for (int i=0; i< plugin.getServer().getOnlinePlayers().length; i++){
+    		  String cnick=plugin.getServer().getOnlinePlayers()[i].getDisplayName();
+    		  if(cnick.startsWith(nick)){
+    			  String mmsg=msg.substring(nick.length()+1);
+    			  event.getPlayer().sendMessage(ChatColor.AQUA + "zprava odeslana");
+    			  plugin.getServer().getOnlinePlayers()[i].sendMessage("PM od: " + ChatColor.AQUA + event.getPlayer().getDisplayName());
+    			  plugin.getServer().getOnlinePlayers()[i].sendMessage(ChatColor.GREEN + mmsg);
+    			  event.setCancelled(true);
+    			  return;
+    		  }
+       	  }
+    	}
 	}
 }
